d267ca0262a76af4a950e3aa7f58ef3b22673920
Alex Star <sonofagronom@gmail.com>
2020-12-04 15:23:34 +0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a1993b795a72a8b3b7029c153d2f6196'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b47ce9b7-023b-4606-a6a1-b9c2b30eac15'
+};
